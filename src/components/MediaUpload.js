7d758f14eedd7b7ce4df4006e8022787f624bd5f
amnonbb <amnonbb@gmail.com>
2019-06-12 15:07:41 +0300

    show progress  multiple upload files

--- src/components/MediaUpload.js
+++ src/components/MediaUpload.js
@@ -5,20 +5,32 @@ import {MTSRV_BACKEND} from "../shared/tools";
 
 class MediaUpload extends Component {
 
-    state = { percent: 0 };
+    state = {
+        progress: {},
+    };
 
     progress = (step, file) => {
+        let {progress} = this.state;
         let count = Math.round(step.percent);
-        //console.log('onProgress', step, file.name);
-        this.setState({percent: count});
+        progress[file.name] = count;
+        this.setState({progress});
     };
 
     uploadDone = (file) => {
+        let {progress} = this.state;
         console.log("Upload done", file);
-        this.setState({percent: 0})
+        delete progress[file.file_name];
+        this.setState({progress})
     };
 
     render() {
+
+        const {progress} = this.state;
+
+        let files_progress = Object.keys(progress).map((id) => {
+            let count = progress[id];
+            return (<Progress key={id} label={id} percent={count} indicating progress='percent' />)
+        });
 
         const props = {
             action: `${MTSRV_BACKEND}/coder/upload`,
@@ -45,10 +57,10 @@ class MediaUpload extends Component {
                         className="upload-block"
                         onSuccess={this.uploadDone}
                         onProgress={this.progress} >
-                        Drop file here or click me
-                        <Progress label='' percent={this.state.percent} indicating progress='percent' />
+                        Drop files here or click me
                     </Upload>
                 </Message>
+                {files_progress}
             </Segment>
         );
     }
